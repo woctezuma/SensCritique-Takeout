a6b9a3b72cb56787057dacfb1f7d2ea34e0f7144
Wok <woctezuma@users.noreply.github.com>
2020-10-28 14:44:56 +0100

    Standardize setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
-from distutils.core import setup
-
-# noinspection PyUnresolvedReferences
 import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='senscritique',
-    packages=['senscritique'],
+    version='0.5.1',
+    author='Wok',
+    author_email='wok@tuta.io',
+    description='SensCritique Takeout on PyPI',
+    keywords=['senscritique', 'takeout', 'takeaway'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woctezuma/SensCritique-Takeout',
+    download_url='https://github.com/woctezuma/SensCritique-Takeout/archive/0.5.1.tar.gz',
+    packages=setuptools.find_packages(),
     install_requires=[
         'beautifulsoup4',
         'requests',
         'lxml',
     ],
-    version='0.5.1',
-    description='SensCritique Takeout on PyPI',
-    long_description='Export your data from SensCritique.com, similarly to Google Takeout.',
-    long_description_content_type='text/x-rst',
-    author='Wok',
-    author_email='wok@tuta.io',
-    url='https://github.com/woctezuma/SensCritique-Takeout',
-    download_url='https://github.com/woctezuma/SensCritique-Takeout/archive/0.5.1.tar.gz',
-    keywords=['senscritique', 'takeout', 'takeaway'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
     classifiers=[
-        'Topic :: Games/Entertainment',
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        'Topic :: Games/Entertainment',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         'Intended Audience :: End Users/Desktop',
         'Natural Language :: French',
     ],
+    python_requires='>=3',
 )
